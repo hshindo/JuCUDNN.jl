d8eb7ecae2a94e4a897b927e8ea48b756d18ae8e
Hiroyuki Shindo <hshindo@outlook.com>
2016-07-20 15:06:09 +0900

    Update pooling

--- src/pooling.jl
+++ src/pooling.jl
@@ -1,55 +1,66 @@
-export PoolingDesc, pooling!, pooling, ∇pooling!
-export CUDNN_POOLING_MAX
-export CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING
-export CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING
+export pooling, pooling!, ∇pooling!
+export
+  CUDNN_POOLING_MAX,
+  CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING,
+  CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING
+export
+  CUDNN_NOT_PROPAGATE_NAN,
+  CUDNN_PROPAGATE_NAN
 
-type PoolingDesc{N}
-  ptr::Ptr{Void}
-  window::NTuple{N,Int}
-  padding::NTuple{N,Int}
-  stride::NTuple{N,Int}
-end
-
-function PoolingDesc{N}(mode, window::NTuple{N,Int}, padding::NTuple{N,Int}, stride::NTuple{N,Int};
-  maxpooling_nanopt=CUDNN_NOT_PROPAGATE_NAN)
+function pooling_desc(mode, window, padding, stride, maxpoolingNanOpt)
+  N = length(window)
   p = Ptr{Void}[0]
   cudnnCreatePoolingDescriptor(p)
   cwindow = Cint[window[i] for i=N:-1:1]
   cpadding = Cint[padding[i] for i=N:-1:1]
   cstride = Cint[stride[i] for i=N:-1:1]
-  cudnnSetPoolingNdDescriptor(p[1], mode, maxpooling_nanopt, N, cwindow, cpadding, cstride)
-  desc = PoolingDesc(p[1], window, padding, stride)
-  finalizer(desc, cudnnDestroyPoolingDescriptor)
-  desc
+  cudnnSetPoolingNdDescriptor(p[1], mode, maxpoolingNanOpt, N, cwindow, cpadding, cstride)
+  p[1]
 end
 
-Base.unsafe_convert(::Type{Ptr{Void}}, desc::PoolingDesc) = desc.ptr
+function pooling!{T}(x::CuArray{T}, window, padding, stride, mode, y::CuArray{T};
+  maxpoolingNanOpt=CUDNN_NOT_PROPAGATE_NAN, alpha=1.0, beta=0.0)
 
-function pooling!{T}(x::CuArray{T}, y::CuArray{T}, poolingdesc; alpha=1.0, beta=0.0)
-  h = gethandle(x.dev)
-  xdesc = TensorDesc(x)
-  ydesc = TensorDesc(y)
+  h = gethandle(device(x))
+  xdesc = tensor_desc(x)
+  poolingdesc = pooling_desc(mode, window, padding, stride, maxpoolingNanOpt)
+  ydesc = tensor_desc(y)
   cudnnPoolingForward(h, poolingdesc, T[alpha], xdesc, x, T[beta], ydesc, y)
+
+  cudnnDestroyTensorDescriptor(xdesc)
+  cudnnDestroyPoolingDescriptor(poolingdesc)
+  cudnnDestroyTensorDescriptor(ydesc)
+  y
 end
 
-function pooling(x::CuArray, poolingdesc; alpha=1.0)
-  N = length(poolingdesc.window)
-  w, p, s = poolingdesc.window, poolingdesc.padding, poolingdesc.stride
+function pooling(x::CuArray, window, padding, stride, mode;
+  maxpoolingNanOpt=CUDNN_NOT_PROPAGATE_NAN, alpha=1.0, beta=0.0)
+
+  N = length(window)
   outdims = Array(Int, N)
   for i = 1:N
-    outdims[i] = (size(x,i) + 2*p[i] - w[i]) ÷ s[i] + 1
+    outdims[i] = (size(x,i) + 2*padding[i] - window[i]) ÷ stride[i] + 1
   end
   y = similar(x, outdims..., size(x,N+1), size(x,N+2))
-  pooling!(x, y, poolingdesc, alpha)
+  pooling!(x, window, padding, stride, mode, y, maxpoolingNanOpt=maxpoolingNanOpt, alpha=alpha, beta=beta)
 end
 
-function ∇pooling!{T}(poolingdesc, y::CuArray{T}, dy::CuArray{T},
-  x::CuArray{T}, dx::CuArray{T}; alpha=1.0, beta=0.0)
-  h = gethandle(x.dev)
-  ydesc = TensorDesc(y)
-  dydesc = TensorDesc(dy)
-  xdesc = TensorDesc(x)
-  dxdesc = TensorDesc(dx)
-  cudnnPoolingBackward(h, poolingdesc, ydesc, y, dydesc, dy, xdesc, x,
+function ∇pooling!{T}(y::CuArray{T}, dy::CuArray{T}, x::CuArray{T},
+  window, padding, stride, mode, dx::CuArray{T};
+  maxpoolingNanOpt=CUDNN_NOT_PROPAGATE_NAN, alpha=1.0, beta=0.0)
+
+  h = gethandle(device(x))
+  poolingdesc = pooling_desc(mode, window, padding, stride, maxpoolingNanOpt)
+  ydesc = tensor_desc(y)
+  dydesc = tensor_desc(dy)
+  xdesc = tensor_desc(x)
+  dxdesc = tensor_desc(dx)
+  cudnnPoolingBackward(h, poolingdesc, T[alpha], ydesc, y, dydesc, dy, xdesc, x,
     T[beta], dxdesc, dx)
+
+  cudnnDestroyPoolingDescriptor(poolingdesc)
+  cudnnDestroyTensorDescriptor(ydesc)
+  cudnnDestroyTensorDescriptor(dydesc)
+  cudnnDestroyTensorDescriptor(xdesc)
+  cudnnDestroyTensorDescriptor(dxdesc)
 end
