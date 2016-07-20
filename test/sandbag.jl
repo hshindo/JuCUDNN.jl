b3842e8d46cc45c03e560f1c1c151db19335e65d
Hiroyuki Shindo <hshindo@outlook.com>
2016-07-20 15:07:49 +0900

    Update test code for pooling

--- test/sandbag.jl
+++ test/sandbag.jl
@@ -8,3 +8,10 @@ x = curand(Float32,10,5,4,3)
 Array(x)
 y = activation!(CUDNN_ACTIVATION_SIGMOID, x, similar(x))
 Array(y)
+
+x = curand(Float32,5,5,1,1)
+y = pooling(x,(3,3),(1,1),(2,2),CUDNN_POOLING_MAX)
+dy=y
+dx=zeros(x)
+∇pooling!(y,dy,x,(3,3),(1,1),(2,2),CUDNN_POOLING_MAX,dx)
+Array(dx)
