64d4dbc311c0e2785d228aeb86a50abe1c3ea1a0
Hiroyuki Shindo <hshindo@outlook.com>
2016-07-29 12:06:38 +0900

    Update sandbag

--- test/sandbag.jl
+++ test/sandbag.jl
@@ -5,7 +5,24 @@ using JuCUDNN
 ndevices()
 
 x = curand(Float32,5,4,3,2)
+hiddensize = 1
+numlayers = 1
+droprate = 0.5
+desc = rnn_desc(x, hiddensize, numlayers, droprate, CUDNN_LINEAR_INPUT, CUDNN_UNIDIRECTIONAL, CUDNN_RNN_RELU)
+
+
+JuCUDNN.cudnnGetVersion()
+
+x = curand(Float32,5,4,3,2)
 x = curand(Float32,1000)
 y = dropout(x, 0.5)
 Array(x)
 Array(y)
+
+x = curand(Float32,5,4,3,2)
+Array(x)
+y, s, ssize, rspace, rsize = dropout(x, 0.5)
+dy=y
+dx=zeros(x)
+∇dropout!(x, dy, 0.5, s, ssize, rspace, rsize, dx)
+Array(dx)
